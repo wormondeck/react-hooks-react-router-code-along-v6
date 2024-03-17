2d25b98754e26d77618dcb77db7dca569cb6dbf4
Ronney Arias <ronneyarias@gmail.com>
2024-03-16 20:24:34 -0500

    complete lab

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+function NavBar() {
+ return (
+  <nav>
+   <NavLink
+    to="/"
+    /* add styling to NavLink */
+    className="nav-link"
+   >
+    Home
+   </NavLink>
+   <NavLink
+    to="/about"
+    className="nav-link"
+   >
+    About
+   </NavLink>
+   <NavLink
+    to="/login"
+    className="nav-link"
+   >
+    Login
+   </NavLink>
+  </nav>
+ );
+};
+
+export default NavBar;
